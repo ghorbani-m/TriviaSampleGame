3f1c7cbbf3fcb947a2ffe6e0bc9c827761029f8c
Mahdi Ghorbani <mahdi.ghorbani@gmail.com>
2019-06-30 00:18:02 +0300

    setStartTime, setEndTime action are added.

--- src/store/match/actions.tsx
+++ src/store/match/actions.tsx
@@ -6,31 +6,59 @@ import { QuestionService } from '../../services/question-service';
 
 export const refreshQuestion: ActionCreator<
   ThunkAction<Promise<any>, IMatchState, null, MatchActions>
-> = () => {
-  return async (dispatch: Dispatch<MatchActions>) => {
-    try {
-      const questions=await QuestionService.getRandomQuestion();
-      dispatch({
-        type: MatchActionTypes.REFRESH_QUESTIOM,
-        payload:questions
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  > = () => {
+    return async (dispatch: Dispatch<MatchActions>) => {
+      try {
+        const questions=await QuestionService.getRandomQuestion();
+        dispatch({
+          type: MatchActionTypes.REFRESH_QUESTIOM,
+          payload:questions
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
 };
 
 export const addUserAnswer: ActionCreator<
   ThunkAction<Promise<any>, IMatchState, null, MatchActions>
-> = (answer:string) => {
-  return async (dispatch: Dispatch<MatchActions>) => {
-    try {
-      dispatch({
-        type: MatchActionTypes.ADD_ANSWER,
-        payload:answer
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  > = (answer:boolean) => {
+    return async (dispatch: Dispatch<MatchActions>) => {
+      try {
+        dispatch({
+          type: MatchActionTypes.ADD_ANSWER,
+          payload:answer
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };  
+};
+export const setStartTime: ActionCreator<
+  ThunkAction<Promise<any>, IMatchState, null, MatchActions>
+  > = (startTime:Date) => {
+    return async (dispatch: Dispatch<MatchActions>) => {
+      try {
+        dispatch({
+          type: MatchActionTypes.SET_STARTTIME,
+          payload:startTime
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };  
+};
+export const setEndTime: ActionCreator<
+  ThunkAction<Promise<any>, IMatchState, null, MatchActions>
+  > = (endTime:Date) => {
+    return async (dispatch: Dispatch<MatchActions>) => {
+      try {
+        dispatch({
+          type: MatchActionTypes.SET_STARTTIME,
+          payload:endTime
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };  
 };
--- src/store/match/reducers.tsx
+++ src/store/match/reducers.tsx
@@ -15,12 +15,23 @@ import {
       case MatchActionTypes.REFRESH_QUESTIOM:
         return {
           question: action.payload,
-          userAnswers:[]
+          userAnswers:[],
+          startTime:new Date(),
         }
       case MatchActionTypes.ADD_ANSWER:
         return {
             ...state,
             userAnswers:[...state.userAnswers,action.payload]
+        }
+      case MatchActionTypes.SET_STARTTIME:
+        return {
+            ...state,
+            startTime:action.payload
+        }
+      case MatchActionTypes.SET_ENDTIME:
+        return {
+            ...state,
+            endTime:action.payload
         }
       default:
         return state
