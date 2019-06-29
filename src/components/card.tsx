5422d6f8261b8e13cd8f6c6d2bf0c1f2ef9cba43
Mahdi Ghorbani <mahdi.ghorbani@gmail.com>
2019-06-30 00:16:41 +0300

    card component is improved.

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,31 +6,43 @@ import {Button} from "./index"
 
 interface Props {
   question:IQuestion,
-  onAnswer(answer:string):void
+  onAnswer(answer:boolean):void
 }
 interface State{
-  loading:boolean
+  answer?:string,
+  buttonIndex:number,
+  answers:string[]
 }
 export default class Card extends Component<Props,State> {
-    onButtonClick(answer:string){
-        this.props.onAnswer(answer);
-
+    state={
+        answer:"",
+        buttonIndex:-1,
+        answers:staticHelper.shuffle([...this.props.question.incorrect_answers,this.props.question.correct_answer])
+    }
+    onButtonClick(answer:string,buttonIndex:number){
+        const{question}=this.props;
+        const result=answer==question.correct_answer;
+        this.setState({answer:question.correct_answer,buttonIndex},()=>{
+            setTimeout(() => {
+                this.props.onAnswer(result);
+            }, 300);
+        });
     }
     render() {
+        const {buttonIndex,answer,answers}=this.state;
         const {question}=this.props;
-        const answers=staticHelper.shuffle([...question.incorrect_answers,question.correct_answer]);
         return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Text style={styles.questionText}>{question.question}</Text>
             </View>
             <View style={{flex:1,paddingBottom:5}}>
-                {answers.map((answer,index)=>
+                {answers.map((answerText,index)=>
                     <View style={{flex:1, padding:5,paddingHorizontal:15}}>
                         <Button key={index} 
-                            title={answer} 
-                            style={styles.button}
-                            onPress={()=>this.onButtonClick(answer)} />
+                            title={answerText} 
+                            style={[styles.button,index===buttonIndex?answerText===answer?styles.success:styles.wrong:{}]}
+                            onPress={()=>!answer?this.onButtonClick(answerText,index):null} />
                     </View>
                 )}
             </View>
@@ -44,5 +56,7 @@ const styles=StyleSheet.create({
     container: {flex:1},
     header:{flex:1,justifyContent:"center",alignItems:"center"},
     questionText: {color:"white",fontSize:18,textAlign:"center",padding:15},
-    button:{borderColor:"#ea80fc",borderWidth:1}
+    button:{borderColor:"#ea80fc",borderWidth:1},
+    success:{backgroundColor:"#43a047"},
+    wrong:{backgroundColor:"#e64a19"}
   });
